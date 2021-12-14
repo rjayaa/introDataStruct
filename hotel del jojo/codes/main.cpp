65a3eb86cd79931f0508e32b3e209059295a7d53
rjayaa <ragil.lelana@binus.ac.id>
2021-12-14 19:13:39 +0700

    no 1 sampai 3

---
+++ hotel del jojo/codes/main.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+import_data_from_file
+struct Datahotel{
+    char customer_name[];
+    int customer_age;
+    char customer_causedeath[];
+    char customer_restingplace[];
+    int customer_diedyear;
+    int room_typeid;
+};
+
+struct Room{
+    int room_name;
+    int room_price;
+
+};
+
+int main(){
+
+
+
+
+    return 0;
+}
